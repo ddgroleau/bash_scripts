76b3ce5fb645f8e0762c5f3d4889edcbac448d72
ddgroleau <ddgroleau@gmail.com>
2021-12-16 08:33:00 -0700

    Updated always-green.sh

--- always-green.sh
+++ always-green.sh
@@ -3,5 +3,6 @@ sudo date > today.txt
 git add . &> /dev/null
 git commit -m "Updated always-green.sh" --quiet
 git push origin master --quiet
+cd -
 
 echo "Pushed new commit to GitHub remote origin."
