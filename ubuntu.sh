a62cf3cd96a02ef5006864de25997158e8f47f46
Dan Groleau <ddgroleau@gmail.com>
2021-12-17 13:09:18 -0700

    Updated always-green.sh

---
+++ ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+my-cmds () (
+echo "Command    Tool"
+echo "-------    -----------"
+echo "ff         Firefox"
+echo "gpo(arg)   Git Add, Commit, Push (Commit Message)"
+echo "spotify    Spotify"
+echo "code(arg)  VS Code (Directory)"
+)
+
+ff () (
+    echo -e "\nOpening Firefox...\n"
+    /bin/firefox 2>&1 &
+)
+
+spotify () (
+    echo -e "\nOpening Spotify...\n"
+    /bin/spotify 2>&1 &
+)
+
+gpo () (
+    echo -e "\nCommitting and pushing your changes to current branch...\n"
+    git add .
+    git commit -m "$1"
+    git push origin $(git branch --show-current)
+    )
+
+src () (
+    source ~/.bashrc
+)
