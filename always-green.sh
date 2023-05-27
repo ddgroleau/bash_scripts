5aedece457d25c33cd94d841f85656715ff1acce
Dan Groleau <ddgroleau@gmail.com>
2023-05-27 10:29:17 -0400

    Update always-green.sh

--- always-green.sh
+++ always-green.sh
@@ -4,7 +4,7 @@ cd ~/repos/bash_scripts
 
 sudo "always-green " >> always-green.txt
 git pull origin master --force
-git commit -am "Updated uuid.txt" --quiet
+git commit -am "Updated always-green.txt" --quiet
 git push origin master --quiet --force
 
 if [ $? -eq 0 ]
